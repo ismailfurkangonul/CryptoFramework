c7da13e248aea7b5a9841ee1238f0ad2d25a704c
ismailfurkangonul <ismailfurkangonul2@gmail.com>
2022-07-16 19:32:49 +0300

    Add project files.

---
+++ CryptoFramework/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace CryptoFramework
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+           
+           
+            Console.WriteLine("Şifrelenecek metni giriniz:enter tusuna basiniz:");
+            string metin= Console.ReadLine();
+
+            Console.WriteLine(Crypto.Sifrele(metin));
+           
+            Console.WriteLine("çözmek istediğiniz şifreli metni giriniz:");
+            string sifreliMetin = Console.ReadLine();
+            Console.WriteLine(Crypto.SifreCoz(sifreliMetin));
+
+            Console.ReadLine();
+        }
+    }
+}
